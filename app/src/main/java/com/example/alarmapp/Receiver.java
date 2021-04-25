bcaefcf15e448f1b4cbec11590a6d6e67e63b6de
esther <esthermiriamgoldberg@outlook.com>
2021-04-24 23:43:57 -0400

    removed extraneous comments

--- app/src/main/java/com/example/alarmapp/Receiver.java
+++ app/src/main/java/com/example/alarmapp/Receiver.java
@@ -7,13 +7,9 @@ import android.media.MediaPlayer;
 
 
 public class Receiver extends BroadcastReceiver {
-    MediaPlayer ring;
+
         @Override
         public void onReceive(Context context, Intent intent) {
-
-                //ring = MediaPlayer.create(context, R.raw.alarm_ring);
-
-                //ring.start();  //this starts the ringing of the alarm
 
                 Intent ringIntent = new Intent(context, RingActivity.class);
                 ringIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
